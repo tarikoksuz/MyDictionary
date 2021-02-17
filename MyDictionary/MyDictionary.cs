81c2c6b7d7f90b99ba9bf73e48b02ad89cbcdafa
tarik oksuz <tarikoksuz@hotmail.com>
2021-02-17 20:21:03 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keysArray;
+        TValue[] valuesArray;
+
+        public MyDictionary()
+        {
+            keysArray = new TKey[0];
+            valuesArray = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKey = keysArray;
+            TValue[] tempValue = valuesArray;
+            keysArray = new TKey[keysArray.Length + 1];
+            valuesArray = new TValue[valuesArray.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keysArray[i] = tempKey[i];
+                valuesArray[i] = tempValue[i];
+            }
+
+            keysArray[keysArray.Length - 1] = key;
+            valuesArray[valuesArray.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return valuesArray.Length; }
+        }
+        public void List()
+        {
+            for (int i = 0; i < keysArray.Length; i++)
+            {
+                Console.WriteLine(keysArray[i].ToString() + valuesArray[i].ToString());
+
+            }
+
+        }
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> gsFutbolcu = new MyDictionary<string, string>();
+
+            gsFutbolcu.Add("Fernando Muslera", " " + "34");
+            gsFutbolcu.Add("Mostafa Muhammed", " " + "23");
+            gsFutbolcu.Add("Henry Onyekuru", " " + "23");
+            gsFutbolcu.Add("Radamel Falcao"," " + "35");
+
+            Console.WriteLine(gsFutbolcu.Count);
+            gsFutbolcu.List();
+
+        }
+    }
+}
